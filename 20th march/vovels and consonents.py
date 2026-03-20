b1636a0460d55d409274edee099c1648908c7716
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-03-20 22:32:37 +0530

    1.⁠ ⁠Count number of vowels and consonants in a string.

---
+++ 20th march/vovels and consonents.py
@@ -0,0 +1,15 @@
+s = input("Enter the string : ")
+vowels = ["a","e","i","o","u","A","E","I","O","U"]
+
+v_count = 0
+c_count = 0
+
+for ch in s:
+    if (ch >= "a" and ch <= "z") or (ch >= "A" and ch <= "Z"):
+        if ch in vowels:
+            v_count += 1
+        else:
+            c_count += 1
+
+print("Count of vowels in string : ", v_count)
+print("Count of consonents in string : ", c_count)
