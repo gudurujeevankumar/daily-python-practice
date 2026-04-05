0e392cbfdc190c142c4fe9750b5709b434e1c87e
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-04-05 22:31:49 +0530

    Remove duplicates from string completed

--- April/5th April/dup.py
+++ April/5th April/dup.py
@@ -1,1 +1,10 @@
 # Remove duplicates from string
+
+my_string = input("Enter the string : ")
+seen = ""
+
+for char in my_string:
+    if char not in seen:
+        seen = seen + char
+
+print(seen)
